2a525b4d3bc73d0dc1a0ef73e64cec61ddf4cea7
Riquwu <ariqbimantoro@gmail.com>
2022-04-27 16:44:57 +0700

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// Import Modules
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// Import Reducers
+import { calculateTotals } from "./features/cart/cartSlice";
+// Import Components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-import { useSelector, useDispatch } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 const App = () => {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,6 +18,7 @@ const App = () => {
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
