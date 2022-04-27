8e554246dd6f789702316a48f5f3cd341604769f
Riquwu <ariqbimantoro@gmail.com>
2022-04-27 22:44:40 +0700

    Add loading logic and dispatch fetch data

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // Import Reducers
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 // Import Components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
 
 const App = () => {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems("Random name"));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading ....</h1>
+      </div>
+    );
+  }
+
   return (
     <main>
       {isOpen && <Modal />}
